5b576d7dfd83de27af97da057673ee4429f8951d
Albert <ratajczakalbert@gmail.com>
2019-01-19 21:08:05 +0100

    Removing global variable WEATHER_DATA

--- weather_station_app/main.py
+++ weather_station_app/main.py
@@ -1,3 +1,7 @@
+""" Albert Ratajczak, 2019
+"""
+import pdb
+
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from pprint import pprint
@@ -12,21 +16,22 @@ print('building service')
 CREDS = Credentials.from_service_account_file(SERVICE_ACCOUNT, scopes=SCOPES)
 SERVICE = build('sheets', 'v4', credentials=CREDS)
 
-WEATHER_DATA = []
 
-def update_weather_data():
-    global WEATHER_DATA
-    sheet = SERVICE.spreadsheets()
-    request = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME)
-    WEATHER_DATA = request.execute()['values']
+def get_weather_data():
+    return SERVICE.spreadsheets().values() \
+           .get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME) \
+           .execute()['values']
 
 
 def recent_weather():
-    temperature = (float(WEATHER_DATA[-1][1])+float(WEATHER_DATA[-1][2])) / 2
-    return {'Date': WEATHER_DATA[-1][0], 'Temperature': str(temperature),
-            'Pressure': WEATHER_DATA[-1][3], 'Humidity': WEATHER_DATA[-1][4]}
+    weather_data = get_weather_data()
+    temperature = (float(weather_data[-1][1])+float(weather_data[-1][2])) / 2
+    return {'Date': weather_data[-1][0],
+            'Temperature': str(temperature),
+            'Pressure': weather_data[-1][3],
+            'Humidity': weather_data[-1][4]}
 
 
 if __name__ == '__main__':
-    update_weather_data()
+    # pdb.set_trace()
     pprint(recent_weather())
