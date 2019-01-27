b8aa0bc95dc1ceed3852eca2c76434aded66df35
Albert <ratajczakalbert@gmail.com>
2019-01-27 18:17:29 +0100

    Ranaming dirs

--- weather-station/template-secret.h
+++ weather-station/template-secret.h
@@ -1,5 +1,5 @@
 // Change the name of this file to secret.h
-// and asign your Google Form ID to constant FORM_ID
+// and assign your Google Form ID to constant FORM_ID
 
 // Google Form ID
 const String FORM_ID = "your google form id";
